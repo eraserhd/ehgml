97b61850db25ce3077b98f69b7eda848f7d918e6
Jason Felice <jason.m.felice@gmail.com>
2012-12-06 12:33:29 -0500

    Add autoconf/automake products

---
+++ acconfig.h
@@ -0,0 +1,8 @@
+/* Define this to enable debug features. */
+#undef DEBUG
+
+/* Define this to disable asserts. */
+#undef NDEBUG
+
+/* Define this if your c++ compiler supports named return values (only g++?) */
+#undef HAVE_NAMED_RETURN_VALUES
